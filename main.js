c9c7648d23c57dfa194a7d1f746b42a56665bb3e
Jonas Santos <jonassoares.live@gmail.com>
2023-11-08 21:00:24 -0300

    Task design review 1.0

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let saveButton = document.querySelector('#btn-task')
 let input = document.querySelector('#itask')
-let res = document.querySelector('#tasks')
+let res = document.querySelector('#task-list')
 let addButton = document.querySelector('#add-button')
 let closeButton = document.querySelector('#close-button')
 let overlay = document.querySelector('#overlay')
@@ -48,18 +48,58 @@ function animationInputPage(animationState) {
   }
 }
 
+function createTaskText(text) {
+  let div = document.createElement('div')
+  div.setAttribute('class', 'task taskAvailable')
+  div.innerText = text + ' '
+  return div
+}
+
+function createDeleteButton() {
+  let deleteButton = document.createElement('img')
+  deleteButton.setAttribute('src', 'img/delete.png')
+  deleteButton.setAttribute('class', 'erase')
+  return deleteButton
+}
+
+function createCheck() {
+  let checkButton = document.createElement('img')
+  let checkedButton = document.createElement('img')
+  checkButton.setAttribute('src', 'img/unchecked.png')
+  checkButton.setAttribute('class', 'check-button')
+  checkTask(checkButton)
+  return checkButton
+}
+
 function createTask(text) {
-  let li = createLi(text)
-  let button = createButton()
-  li.appendChild(button)
-  res.appendChild(li)
+  let task = createTaskText(text)
+  let deleteButton = createDeleteButton()
+  let checkButton = createCheck()
+  task.insertBefore(checkButton, task.firstChild)
+  task.appendChild(deleteButton)
+  res.appendChild(task)
   saveTask()
   clearInput()
 }
 
+function checkTask(checkButton) {
+  checkButton.addEventListener('click', () => {
+    checkButton.classList.add('checked')
+    setTimeout(() => {
+      if (checkButton.src.includes('unchecked.png')) {
+        checkButton.src = 'img/checked.png'
+      } else {
+        checkButton.src = 'img/unchecked.png'
+      }
+
+      checkButton.classList.remove('checked')
+    }, 300);
+  })
+}
+
 function saveTask() {
   const tasks = []
-  const task = res.querySelectorAll('li')
+  const task = res.querySelectorAll('div')
   task.forEach((item) => {
     let text = item.textContent
     text = text.replace('Apagar', '')
@@ -78,19 +118,6 @@ function loadTasks() {
 function clearInput() {
   input.value = ''
   input.focus()
-}
-
-function createLi(text) {
-  let li = document.createElement('li')
-  li.innerText = text + ' '
-  return li
-}
-
-function createButton() {
-  let button = document.createElement('button')
-  button.setAttribute('class', 'erase')
-  button.innerText = 'Apagar'
-  return button
 }
 
 loadTasks()
