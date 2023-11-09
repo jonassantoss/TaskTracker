5aa564b1df877cf304fecae40b46e0e738608c6a
Jonas Santos <jonassoares.live@gmail.com>
2023-11-09 18:44:27 -0300

    new feature: edit tasks

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-let saveButton = document.querySelector('#btn-task')
-let input = document.querySelector('#itask')
+let saveCreateButton = document.querySelector('#btn-create-task')
+let saveEditButton = document.querySelector('#btn-edit-task')
+let inputCreate = document.querySelector('#i-create-task')
+let inputEdit = document.querySelector('#i-edit-task')
+let input = document.querySelector('.input')
 let res = document.querySelector('#task-list')
 let addButton = document.querySelector('#add-button')
-let closeButton = document.querySelector('#close-button')
-let overlay = document.querySelector('#overlay')
-let inputPage = document.querySelector('.task-input-page')
+let overlayCreateTask = document.querySelector('#overlayCreateTask')
+let overlayEditTask = document.querySelector('#overlayEditTask')
+let taskCreatePage = document.querySelector('#task-input-page')
+let taskEditPage = document.querySelector('#task-edit-page')
+let closeCreatePage = document.querySelector('#close-createTask-button')
+let closeEditPage = document.querySelector('#close-editTask-button')
 
-addButton.addEventListener('click', () => {
-  animationInputPage(true)
+let taskToEdit;
+
+addButton.addEventListener('click', (e) => {
+  handlePageClose(true, overlayCreateTask, taskCreatePage)
 })
 
-input.addEventListener('keypress', (e) => {
+inputCreate.addEventListener('keypress', (e) => {
   if (e.keyCode == 13) {
-    if (!input.value) return;
-    createTask(input.value)
-    animationInputPage(false)
+    if (!inputCreate.value) return;
+    createTask(inputCreate.value)
+    handlePageClose(false, overlayEditTask, taskEditPage)
   }
 })
 
-saveButton.addEventListener('click', (e) => {
-  if (!input.value) return
-  createTask(input.value)
-  animationInputPage(false)
+saveCreateButton.addEventListener('click', (e) => {
+  if (!inputCreate.value) return
+  createTask(inputCreate.value)
+  handlePageClose(false, overlayEditTask, taskEditPage)
 })
 
-closeButton.addEventListener('click', () => {
-  animationInputPage(false)
+inputEdit.addEventListener('keypress', (e) => {
+  if (e.keyCode == 13) {
+    if (!inputEdit.value) return;
+    saveEditTask()
+    handlePageClose(false, overlayEditTask, taskEditPage)
+  }
+})
+
+saveEditButton.addEventListener('click', (e) => {
+  if (!inputEdit.value || !taskToEdit) return
+
+  saveEditTask()
+
+  handlePageClose(false, overlayEditTask, taskEditPage);
+})
+
+closeEditPage.addEventListener('click', (e) => {
+  handlePageClose(false, overlayEditTask, taskEditPage)
+})
+
+closeCreatePage.addEventListener('click', (e) => {
+  handlePageClose(false, overlayCreateTask, taskEditPage)
 })
 
 res.addEventListener('click', (e) => {
@@ -36,23 +64,29 @@ res.addEventListener('click', (e) => {
   }
 })
 
-function animationInputPage(animationState) {
+function handlePageClose(animationState, overlay, taskPage) {
   if (animationState) {
     overlay.style.display = 'flex'
     overlay.style.opacity = 1
-    inputPage.style.transform = 'scale(1)'
+    taskPage.style.transform = 'scale(1)'
   } else {
     overlay.style.opacity = 0
-    inputPage.style.transform = 'scale(0.8)'
+    taskPage.style.transform = 'scale(0.8)'
     setTimeout(() => overlay.style.display = 'none', 500)
   }
 }
 
 function createTaskText(text) {
-  let div = document.createElement('div')
-  div.setAttribute('class', 'task taskAvailable')
-  div.innerText = text + ' '
-  return div
+  let content = document.createElement('p')
+  content.setAttribute('class', 'taskContent')
+  content.innerText = text + ' '
+  return content
+}
+
+function createFrameTask() {
+  let frame = document.createElement('div')
+  frame.setAttribute('class', 'task')
+  return frame
 }
 
 function createDeleteButton() {
@@ -62,20 +96,32 @@ function createDeleteButton() {
   return deleteButton
 }
 
-function createCheck() {
+function createCheckButton() {
   let checkButton = document.createElement('img')
-  let checkedButton = document.createElement('img')
   checkButton.setAttribute('src', 'img/unchecked.png')
   checkButton.setAttribute('class', 'check-button')
   checkTask(checkButton)
   return checkButton
 }
 
+function createEditButton() {
+  let editButton = document.createElement('img')
+  editButton.setAttribute('src', 'img/edit.png')
+  editButton.setAttribute('class', 'edit-button')
+  openEditPage(editButton)
+
+  return editButton
+}
+
 function createTask(text) {
-  let task = createTaskText(text)
+  let task = createFrameTask()
+  let content = createTaskText(text)
   let deleteButton = createDeleteButton()
-  let checkButton = createCheck()
+  let checkButton = createCheckButton()
+  let editButton = createEditButton()
   task.insertBefore(checkButton, task.firstChild)
+  task.appendChild(content)
+  task.appendChild(editButton)
   task.appendChild(deleteButton)
   res.appendChild(task)
   saveTask()
@@ -84,12 +130,17 @@ function createTask(text) {
 
 function checkTask(checkButton) {
   checkButton.addEventListener('click', () => {
+    let task = checkButton.parentNode
     checkButton.classList.add('checked')
     setTimeout(() => {
       if (checkButton.src.includes('unchecked.png')) {
         checkButton.src = 'img/checked.png'
+        task.style.backgroundColor = '#1de78262'
+        task.style.textDecoration = 'line-through'
       } else {
         checkButton.src = 'img/unchecked.png'
+        task.style.backgroundColor = '#1dc9e74f'
+        task.style.textDecoration = 'none'
       }
 
       checkButton.classList.remove('checked')
@@ -97,12 +148,15 @@ function checkTask(checkButton) {
   })
 }
 
+function saveEditTask() {
+  taskToEdit.textContent = inputEdit.value
+}
+
 function saveTask() {
   const tasks = []
   const task = res.querySelectorAll('div')
   task.forEach((item) => {
     let text = item.textContent
-    text = text.replace('Apagar', '')
     tasks.push(text)
   })
   let tasksJSON = JSON.stringify(tasks)
@@ -113,6 +167,13 @@ function loadTasks() {
   let tasksJSON = localStorage.getItem('Tarefas')
   let tasks = JSON.parse(tasksJSON)
   tasks.forEach((item) => createTask(item))
+}
+
+function openEditPage(editButton) {
+  editButton.addEventListener('click', (e) => {
+    handlePageClose(true, overlayEditTask, closeEditPage)
+    taskToEdit = editButton.previousElementSibling
+  })
 }
 
 function clearInput() {
